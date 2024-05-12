e2954b01ad05700dd81722e691a0151f1abfe897
Arafat Ali <arafatx@users.noreply.github.com>
2024-05-12 09:26:40 +0800

    Update bing.py
    Add input number of time to run script instead of infinite run to avoid getting banned in the future.

--- bing.py
+++ bing.py
@@ -5,7 +5,7 @@ import json
 import time
 
 isOn = False
-
+count = 0 
 
 def select_random_word_txt():
     with open("words.txt", "r") as file:
@@ -13,13 +13,11 @@ def select_random_word_txt():
         random_word = random.choice(words)
         return random_word
 
-
 def select_random_word_json():
     with open("words.json", "r") as file:
         words = json.load(file)
         random_word = random.choice(words)
         return random_word
-
 
 def copy_random_word():
     random_word = select_random_word_json()
@@ -27,7 +25,6 @@ def copy_random_word():
     keyboard.press_and_release("ctrl+a")
     keyboard.press_and_release("ctrl+v")
     keyboard.press_and_release("enter")
-
 
 def switch():
     global isOn
@@ -37,23 +34,23 @@ def switch():
     else:
         print("Auto copy is off")
 
-
 def auto_copy():
     keyboard.press_and_release("ctrl+w")
     keyboard.press_and_release("ctrl+t")
     time.sleep(1)
     copy_random_word()
 
-
 if __name__ == "__main__":
-
+    number_of_times = int(input("Enter the number of times to run: "))
     keyboard.add_hotkey("ctrl+b", switch)
-
-    print(
-        "Hotkeys inintialised\nPress 'ctrl+b' to copy a random word to your clipboard."
-    )
-
+    print("Hotkeys initialized\nPress 'ctrl+b' to start/stop auto-copying a random word to your clipboard.")
     while True:
-        while isOn:
+        while isOn and count < number_of_times:
             auto_copy()
             time.sleep(6)
+            count += 1
+        if count >= number_of_times:
+            print("Completed")
+            break
+    input("Press Enter to exit...")
+
